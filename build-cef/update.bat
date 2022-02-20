5b750bf40a5295091591d5c8a30cc3d3b26a6ce2
John Torjo <john.groups@torjo.com>
2022-02-20 11:06:17 +0200

    ss

--- build-cef/update.bat
+++ build-cef/update.bat
@@ -1,3 +1,5 @@
+rem get automate-git.py from https://bitbucket.org/chromiumembedded/cef/raw/master/tools/automate/automate-git.py
+
 c:
 cd C:\code\chromium_git
 
@@ -5,4 +7,4 @@ set GN_DEFINES=is_official_build=true use_thin_lto=false proprietary_codecs=true
 
 set GN_ARGUMENTS=--ide=vs2019 --sln=cef --filters=//cef/*
 rem python ..\automate\automate-git.py --download-dir=c:\code\chromium_git --branch=4844 --minimal-distrib --client-distrib --force-clean --x64-build
-python ..\automate\automate-git.py --download-dir=c:\code\chromium_git --branch=4844 --force-clean --x64-build
+python ..\automate\automate-git.py --download-dir=c:\code\chromium_git --branch=4844 --force-clean --no-archive --x64-build
