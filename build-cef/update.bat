d8ccdf26c6a5a5e03fc5452c6cc042cf563e7ec7
John Torjo <john.groups@torjo.com>
2022-02-20 10:26:10 +0200

    added scripts to build CEF

---
+++ build-cef/update.bat
@@ -0,0 +1,5 @@
+set GN_DEFINES=is_official_build=true use_thin_lto=false proprietary_codecs=true ffmpeg_branding=Chrome media_use_ffmpeg=true media_use_libvpx=true  rtc_use_h264=true 
+
+set GN_ARGUMENTS=--ide=vs2019 --sln=cef --filters=//cef/*
+rem python ..\automate\automate-git.py --download-dir=c:\code\chromium_git --branch=4844 --minimal-distrib --client-distrib --force-clean --x64-build
+python ..\automate\automate-git.py --download-dir=c:\code\chromium_git --branch=4844 --force-clean --x64-build
